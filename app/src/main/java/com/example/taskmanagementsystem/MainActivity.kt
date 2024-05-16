4778df17202c368a62fdec825686d615deeb185a
Yethmi Gamage <yethmigamage@gmail.com>
2024-05-16 19:40:33 +0530

    Alignment changed

--- app/src/main/java/com/example/taskmanagementsystem/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagementsystem/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.taskmanagementsystem
 
-
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -45,15 +43,6 @@ class MainActivity : AppCompatActivity(), TaskAdapter.OnItemClickListener {
         findViewById<Button>(R.id.add).setOnClickListener {
             addTask()
         }
-        findViewById<Button>(R.id.view).setOnClickListener {
-            displayTasks()
-        }
-//        findViewById<Button>(R.id.edit).setOnClickListener {
-//            updateTask()
-//        }
-        findViewById<Button>(R.id.delete).setOnClickListener {
-            deleteTask()
-        }
 
         // Observe tasks from ViewModel
         taskViewModel.allTasks.observe(this) { tasks ->
@@ -65,10 +54,6 @@ class MainActivity : AppCompatActivity(), TaskAdapter.OnItemClickListener {
         adapter = TaskAdapter(emptyList(), this::deleteTaskFromAdapter, this)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
-    }
-    private fun deleteTaskFromAdapter(task: Task) {
-        taskViewModel.deleteTask(task.taskId)
-        showToast("Task deleted successfully")
     }
 
     private fun addTask() {
@@ -86,49 +71,6 @@ class MainActivity : AppCompatActivity(), TaskAdapter.OnItemClickListener {
             showToast("Please fill all the fields")
         }
     }
-
-    private fun displayTasks() {
-        taskViewModel.allTasks.observe(this) { tasks ->
-            // This observer will update the RecyclerView adapter whenever the tasks change.
-            adapter.updateTasks(tasks)
-            if (tasks.isEmpty()) {
-                showToast("No tasks found.")
-            }
-        }
-    }
-
-
-//    private fun updateTask() {
-//        val dialogBuilder = AlertDialog.Builder(this)
-//        val inflater = layoutInflater
-//        val dialogView = inflater.inflate(R.layout.update_dialoge, null)
-//        dialogBuilder.setView(dialogView)
-//
-//        val edtTaskId = dialogView.findViewById<EditText>(R.id.updateTaskId)
-//        val edtTaskName = dialogView.findViewById<EditText>(R.id.updateTaskName)
-//        val edtTaskDescription = dialogView.findViewById<EditText>(R.id.updateTaskDescription)
-//        val edtTaskPriority = dialogView.findViewById<EditText>(R.id.updateTaskPriority)
-//        val edtTaskDeadline = dialogView.findViewById<EditText>(R.id.updateTaskDeadline)
-//
-//        dialogBuilder.setTitle("Update Task")
-//        dialogBuilder.setPositiveButton("Update") { _, _ ->
-//            val taskId = edtTaskId.text.toString().toIntOrNull()
-//            val taskName = edtTaskName.text.toString()
-//            val taskDescription = edtTaskDescription.text.toString()
-//            val taskPriority = edtTaskPriority.text.toString().toIntOrNull() ?: 0
-//            val taskDeadline = edtTaskDeadline.text.toString()
-//
-//            if (taskId != null && taskName.isNotEmpty() && taskDescription.isNotEmpty() && taskPriority > 0 && taskDeadline.isNotEmpty()) {
-//                val task = Task(taskId, taskName, taskDescription, taskPriority, taskDeadline)
-//                taskViewModel.updateTask(task)
-//                showToast("Task updated successfully")
-//            } else {
-//                showToast("Please fill all the fields correctly")
-//            }
-//        }
-//        dialogBuilder.setNegativeButton("Cancel") { dialog, _ -> dialog.dismiss() }
-//        dialogBuilder.show()
-//    }
 
     @SuppressLint("InflateParams")
     private fun showUpdateDialog(task: Task) {
@@ -168,26 +110,9 @@ class MainActivity : AppCompatActivity(), TaskAdapter.OnItemClickListener {
         showToast("Task updated successfully")
     }
 
-
-    private fun deleteTask() {
-        val dialogBuilder = AlertDialog.Builder(this)
-        val dialogView = layoutInflater.inflate(R.layout.delete_dialoge, null)
-        dialogBuilder.setView(dialogView)
-
-        val edtTaskId = dialogView.findViewById<EditText>(R.id.deleteTaskId)
-
-        dialogBuilder.setTitle("Delete Task")
-        dialogBuilder.setPositiveButton("Delete") { _, _ ->
-            val taskId = edtTaskId.text.toString().toIntOrNull()
-            if (taskId != null) {
-                taskViewModel.deleteTask(taskId)
-                showToast("Task deleted successfully")
-            } else {
-                showToast("Invalid Task ID")
-            }
-        }
-        dialogBuilder.setNegativeButton("Cancel") { dialog, _ -> dialog.dismiss() }
-        dialogBuilder.show()
+    private fun deleteTaskFromAdapter(task: Task) {
+        taskViewModel.deleteTask(task.taskId)
+        showToast("Task deleted successfully")
     }
 
     private fun showToast(message: String) {
